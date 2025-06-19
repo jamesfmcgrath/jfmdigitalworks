c96be86f23b77b75ed3e16ca527761e2dede29ec
James McGrath <james@mcgrathmedia.com>
2025-06-19 15:23:11 +0100

    Add olivero subtheme

---
+++ web/themes/custom/jfmdw/js/jfmdw.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * JFM Digital Works Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.jfmdw = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
